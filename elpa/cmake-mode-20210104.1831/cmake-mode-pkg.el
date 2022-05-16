b58e836584e87aa858db6b51f47d67d0700c9b3f
Liang <lzl@powsell.com>
2022-05-16 14:11:42 +0800

    modify

--- elpa/cmake-mode-20210104.1831/cmake-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from cmake-mode.el  -*- no-byte-compile: t -*-
-(define-package "cmake-mode" "20210104.1831" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "47cc20fc67357f796b1a51583a9e182bf78bfb69")
